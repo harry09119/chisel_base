a512263ba791156e22ae1e6f617b36cecc1c0261
harry09119 <harry09119@gmail.com>
2024-03-09 16:06:05 +0900

    2024/03/09

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ echo "Running SBT"
 id="harry09119"
 token="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"
 
-today=$(date +"%Y/%m/%d - %H:%M:%S")
+today="$(date +"%Y/%m/%d")"
 
 package=uvp_array
 testname=TMP_Test
@@ -16,5 +16,5 @@ sbt testOnly $package.$testname
 
 echo "Add"
 git add . 
-echo "commit"
-git commit -m today
+echo "Commit"
+git commit -m $today
